fdc0ee2a2dd2cbebec75d93f64620e4558325bbc
PLsergent <papa.louis59@gmail.com>
2019-11-08 15:04:37 +0100

    mongodb done

--- src/index.php
+++ src/index.php
@@ -1,4 +1,25 @@
 <?php
 
-$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-echo"<h1>Hello world!</h1>";
+try {
+
+    $mng = new MongoDB\Driver\Manager("mongodb://mongodb:27017");
+    $query = new MongoDB\Driver\Query([]); 
+     
+    $rows = $mng->executeQuery("init.init", $query);
+    
+    foreach ($rows as $row) {
+    
+        echo "$row->name $row->lastname\n";
+    }
+
+} catch (MongoDB\Driver\Exception\Exception $e) {
+
+    $filename = basename(__FILE__);
+    
+    echo "The $filename script has experienced an error.\n"; 
+    echo "It failed with the following exception:\n";
+    
+    echo "Exception:", $e->getMessage(), "\n";
+    echo "In file:", $e->getFile(), "\n";
+    echo "On line:", $e->getLine(), "\n";       
+}
